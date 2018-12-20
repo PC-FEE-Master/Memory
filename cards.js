ecab4d91a65bfc331c4602ba0345a2fec54d72e4
kscottwordsmith <kscottwordsmith@gmail.com>
2018-12-20 14:05:39 -0800

    day one

---
+++ cards.js
@@ -0,0 +1,25 @@
+var cardList = [
+{
+	source: `<i class="fas fa-bell facedown"></i>`,
+	facedown: true,
+}, 
+{
+	source: `<i class="fas fa-bell facedown"></i>`,
+	facedown: true,
+}, 
+{
+	source: `<i class="fas fa-skull-crossbones facedown"></i>`,
+	facedown: true,
+},
+{
+	source: `<i class="fas fa-skull-crossbones"></i>`,
+	facedown: true,
+},
+{
+	source: `<i class="fas fa-couch"></i>`,
+	facedown: true,
+},
+{
+	source: `<i class="fas fa-couch"></i>`,
+	facedown: true,
+}]
